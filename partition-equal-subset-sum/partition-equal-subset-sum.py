cb37bb7691e5cbe48efad22f1635fd640910015b
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2021-12-13 16:18:59 +0530

    Time: 1968 ms (39.96%), Space: 313.4 MB (5.25%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.py
+++ partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -4,18 +4,32 @@ class Solution:
         if totalSum % 2 == 1:
             return False
         n = len(nums)
-        totalSum //= 2
-        dp = [[False for j in range(totalSum + 1)] for i in range(n+1)]
-        dp[0][0] = True
+        target = totalSum // 2
+        dp = {}
         
-        for i in range(1, n+1):
-            for j in range(1, totalSum + 1):
-                if nums[i-1] <= j:
-                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]]
-                else:
-                    dp[i][j] = dp[i-1][j]
+        def search(i, runningSum):
+            if runningSum == target:
+                return True
+            if runningSum > target or i >= n:
+                return False
+            if (i, runningSum) in dp:
+                return dp[i, runningSum]
+            dp[i, runningSum] = search(i+1, runningSum) or search(i+1, runningSum + nums[i])
+            return dp[i, runningSum]
         
-        return dp[n][totalSum]
+        return search(0, 0)
+        
+#         dp = [[False for j in range(totalSum + 1)] for i in range(n+1)]
+#         dp[0][0] = True
+        
+#         for i in range(1, n+1):
+#             for j in range(1, totalSum + 1):
+#                 if nums[i-1] <= j:
+#                     dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]]
+#                 else:
+#                     dp[i][j] = dp[i-1][j]
+        
+#         return dp[n][totalSum]
         
         
         
