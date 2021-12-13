e929cfd854f6711d9a48b77e92bff6c39f9e56e1
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2021-12-14 00:27:31 +0530

    Time: 56 ms (75.23%), Space: 14.3 MB (96.02%) - LeetHub

---
+++ delete-and-earn/delete-and-earn.py
@@ -0,0 +1,23 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        nums.sort()
+        values = []
+        n = len(nums)
+        i = 0
+        
+        while i<n:
+            j = i
+            while j+1 < n and nums[j+1] == nums[j]:
+                j += 1
+            if len(values) == 0 or nums[j]-1 == nums[i-1]:
+                values.append((j-i+1) * nums[i])
+            else:
+                values.extend([0, (j-i+1) * nums[i]])
+            i = j + 1
+        
+        
+        dp =[0] * (len(values)+1)
+        for index in range(1, len(values) + 1):
+            dp[index] = max(dp[index-1], dp[index-2] + values[index-1])
+        
+        return dp[len(values)]
