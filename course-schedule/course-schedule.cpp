50670307dcfed4d79e04bc39eb15cc3bc065734e
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2021-12-23 14:15:43 +0530

    Time: 20 ms (75.78%), Space: 13.3 MB (80.93%) - LeetHub

---
+++ course-schedule/course-schedule.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> degree(numCourses, 0);
+        vector<int> visited(numCourses, false);
+        vector<vector<int>> graph(numCourses);
+        
+        for(vector<int>& require : prerequisites){
+            int course = require[0];
+            int preCourse = require[1];
+            graph[preCourse].push_back(course);
+            degree[course] ++ ;
+        }
+        
+        std::queue<int> queue;  // will store the independent courses at any moment
+        for(int i = 0; i < numCourses; i++){
+            int deg = degree[i];
+            if(deg == 0)        // if the current course is independent, push into queue
+                queue.push(i);
+        }
+        
+        while(!queue.empty()){
+            int course = queue.front(); queue.pop();
+            visited[course] = true;     // mark this course as done
+            for(int dependent : graph[course]){
+                degree[dependent]--;
+                if(degree[dependent] == 0)
+                    queue.push(dependent);
+            }
+        }
+        
+        for(int i = 0; i < numCourses; i++)
+            if(visited[i] == false)
+                return false;
+        
+        return true;
+    }
+};
