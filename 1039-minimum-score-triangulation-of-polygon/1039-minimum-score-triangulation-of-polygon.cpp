ccb9458d3f22f729176a8119c0ce7e6b8f4f1e11
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-01-09 23:27:15 +0530

    Time: 131 ms (5.97%), Space: 8.6 MB (34.93%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -4,18 +4,19 @@ public:
         int n = values.size();
         vector<vector<int>> dp(n+1, vector<int>(n+1));
         // dp[i][j] : 
-        // minimum score for triangle formed with ith and jth vertex,
-        // with the third vertex in between i and j
+        // minimum score for triangle formed with (i-1)th and jth vertex,
+        // with the third vertex in between i-1 and j
         
-        for(int len = 2; len < n; len++){
-            for(int i = 1; i + len - 1 < n; i++){
-                int j = i + len - 1;
+        for(int points = 2; points < n; points++){ // points selected at each iteration
+            for(int i = 1; i + points - 1 < n; i++){ // the endpoint j should be less than n
+                int j = i + points - 1;
                 dp[i][j] = INT_MAX;
                 for(int k = i; k<j; k++){
                     int value = dp[i][k] + dp[k+1][j];
                     value += (values[i-1] * values[k] * values[j]);
                     dp[i][j] = min(dp[i][j], value);
                 }
+                cout << i << " " << j << "\t" << dp[i][j] << endl;
             }
         }
         
