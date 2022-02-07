33efdd2da03534f0f860be51780af6ed7c9b7ff9
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-02-07 20:23:50 +0530

    Time: 8 ms (17.39%), Space: 6.6 MB (67.00%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         char xored = 0;
-        for(char c : s)
-            xored = xored ^ c;
-        for(char c : t)
-            xored = xored ^ c;
+        for(int i = 0; i < s.length(); i++)
+            xored = xored ^ s[i];
+        for(int i = 0; i < t.length(); i++)
+            xored = xored ^ t[i];
         return xored;
     }
 };
