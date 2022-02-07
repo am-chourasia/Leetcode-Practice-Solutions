7d0b9c54fbea52c2d2b352c8062c9a92fc425398
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-02-07 20:23:07 +0530

    Time: 3 ms (78.44%), Space: 6.5 MB (98.61%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        
-        for(int i = 0; i < s.length(); i++){
-            if(s[i] != t[i])
-                return t[i];
-        }
-        return t.back();
+        char xored = 0;
+        for(char c : s)
+            xored = xored ^ c;
+        for(char c : t)
+            xored = xored ^ c;
+        return xored;
     }
 };
