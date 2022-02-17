5452e46fcd15bbfd6742fb4f6bc158f480641ef4
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-02-18 00:09:44 +0530

    Time: 4 ms (26.06%), Space: 5.7 MB (90.39%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,25 +1,12 @@
 class Solution {
-    bool isSet(int n, int i){
-        return n & (1 << i);
-    }
 public:
     bool checkPowersOfThree(int n) {
-        vector<int> powers(15);
-        int element = 1;
-        for(int i = 0; i < 15; i++){
-            powers[i] = element;
-            element *= 3;
+        while(n > 0){
+            int rem = n % 3;
+            if(rem == 2)
+                return false;
+            n = n/3;
         }
-        
-        for(int i = 0; i < (1 << 15); i++){
-            int k = 0;
-            for(int j = 0; j < 15; j++){
-                if(isSet(i, j))
-                    k += powers[j];
-            }
-            if(k == n)
-                return true;
-        }
-        return false;
+        return true;
     }
 };
