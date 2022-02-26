0b66bd83356690a00da719beaf4f4bc162651a7a
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-02-26 23:06:40 +0530

    Time: 64 ms (33.09%), Space: 47.8 MB (47.14%) - LeetHub

---
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int shortestPathLength(int[][] graph) {
+        int n = graph.length;
+        if(n == 1)
+            return 0;
+        
+        int allTraversed = (1 << n) - 1;
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+        Set<Pair<Integer, Integer>> visited_pair = new HashSet<>();
+        
+        for(int i = 0; i < n; i++){
+            Pair<Integer, Integer> p = new Pair(i, (1 << i));
+            queue.add(p);
+            visited_pair.add(p);
+        }
+        
+        int level = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                Pair<Integer, Integer> p = queue.poll();
+                int currentNode = p.getKey();
+                int traversed = p.getValue();
+                for(int neighbour : graph[currentNode]){
+                    int newTraversed = traversed | (1 << neighbour);
+                    if(newTraversed == allTraversed)
+                        return level + 1;
+                    Pair<Integer, Integer> k = new Pair<Integer, Integer>(neighbour, newTraversed);
+                    if(!visited_pair.contains(k)){
+                        visited_pair.add(k);
+                        queue.add(k);                  
+                    }
+                }
+            }
+            level += 1;
+        }
+        return level;
+    }
+}
