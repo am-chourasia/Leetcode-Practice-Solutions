abc068c6402322c8b13dba0dc2bfeb3ebade926f
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-02-28 22:46:39 +0530

    Time: 5 ms (13.39%), Space: 6.9 MB (62.02%) - LeetHub

---
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> ans;
+        int n = nums.size();
+        if(n == 0)
+            return ans;
+        
+        ans.push_back(to_string(nums[0]));
+        
+        for(int i = 1; i < n; i++){
+            int prev = nums[i - 1];
+            if(prev + 1 != nums[i])
+                ans.push_back(to_string(nums[i]));
+            else if(i == n-1 or nums[i] + 1 != nums[i + 1])
+                ans.back() += "->" + to_string(nums[i]);
+        }
+        
+        return ans;
+    }
+};
