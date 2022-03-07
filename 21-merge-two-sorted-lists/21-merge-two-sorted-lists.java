bfb5928e4fcc0827b690b4096a9dc8863ee5c584
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-03-08 00:24:25 +0530

    Time: 7 ms (83.62%), Space: 14.8 MB (45.55%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -1,51 +1,54 @@
 /**
  * Definition for singly-linked list.
- * public class ListNode {
+ * struct ListNode {
  *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null)
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == nullptr)
             return list2;
-        if(list2 == null)
+        if(list2 == nullptr)
             return list1;
-        ListNode it1 = list1, it2 = list2;
-        ListNode head = null;
-        if(it1.val < it2.val){
+        ListNode *it1, *it2;
+        it1 = list1;
+        it2 = list2;
+        ListNode *head = nullptr;
+        if(it1->val < it2->val){
             head = it1;
-            it1 = it1.next;
+            it1 = it1->next;
         } else {
             head = it2;
-            it2 = it2.next;
+            it2 = it2->next;
         }
         
-        ListNode node = head;
+        ListNode* node = head;
         
-        while(it1 != null && it2 != null){
-            if(it1.val < it2.val){
-                node.next = it1;
-                it1 = it1.next;
+        while(it1 != nullptr and it2 != nullptr){
+            if(it1->val < it2->val){
+                node->next = it1;
+                it1 = it1->next;
             } else {
-                node.next = it2;
-                it2 = it2.next;
+                node->next = it2;
+                it2 = it2->next;
             }
-            node = node.next;
+            node = node->next;
         }
-        while(it1 != null){
-            node.next = it1;
-            it1 = it1.next;
-            node = node.next;
+        while(it1 != nullptr){
+            node->next = it1;
+            it1 = it1->next;
+            node = node->next;
         }
-        while(it2 != null){
-            node.next = it2;
-            it2 = it2.next;
-            node = node.next;
+        while(it2 != nullptr){
+            node->next = it2;
+            it2 = it2->next;
+            node = node->next;
         }
         return head;
     }
-}
+};
