323fef791bea4578a394b6bf59adb676ba8fcf26
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-03-21 11:41:59 +0530

    Time: 151 ms (49.43%), Space: 31.1 MB (79.14%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        stack<int> openings;
         int len = s.length();
-        for(int i = 0; i < len; i++){
-            if(s[i] == '[')
-                openings.push(i);
-        }
-        
-        int open = 0;
-        int swaps = 0;
+        int mismatch = 0;
         
         for(int i = 0; i < len; i++){
-            if(s[i] == '[')
-                open++;
-            else if(open <= 0){
-                int idx = openings.top();
-                openings.pop();
-                swap(s[i], s[idx]);
-                swaps++;
-                open++;
-            } else 
-                open--;
+            char element = s[i];
+            if(element == '[')
+                mismatch++;
+            else if(mismatch > 0)
+                mismatch--;
         }
         
-        return swaps;
+        return (mismatch + 1) / 2;
     }
 };
