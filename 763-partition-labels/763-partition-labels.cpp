90aaf4eb56a5790d4571015d3f98e7fd9a4d0ae1
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-03-21 10:48:45 +0530

    Time: 7 ms (41.00%), Space: 6.5 MB (96.62%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> last(26, -1);
+        int len = s.length();
+        
+        for(int i = 0; i < len; i++){
+            int idx = s[i] - 'a';
+            last[idx] = i;
+        }
+        
+        vector<int> ans;
+        
+        for(int i = 0; i < len; i++){
+            int start = i;
+            int end = last[s[i] - 'a'];
+            while(i < end){
+                int idx = s[i] - 'a';
+                if(last[idx] > end)
+                    end = last[idx];
+                i++;
+            }
+            ans.push_back(end - start + 1);
+        }
+        
+        return ans;
+    }
+};
