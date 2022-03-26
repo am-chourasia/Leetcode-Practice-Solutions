eeaf66e432a21484026aec33490a4a049f807ee9
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-03-27 00:16:16 +0530

    Time: 1 ms (67.73%), Space: 42.8 MB (71.76%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.java
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.java
@@ -8,19 +8,11 @@ class Solution {
             if(nums[idx] <= prev){
                 if(already_removed)
                     return false;
-                if(idx + 1 == len)
-                    break;
                 
-                boolean current_removed = prev < nums[idx + 1];
                 boolean prev_removed = idx - 2 < 0 || nums[idx-2] < nums[idx];
                 already_removed = true;
-                
-                if(current_removed)
-                    prev = prev;
-                else if(prev_removed)
+                if(prev_removed)
                     prev = nums[idx];
-                else
-                    return false;
             } 
             else
                 prev = nums[idx];
