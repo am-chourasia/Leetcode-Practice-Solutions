6455da50068f09b30128921ba5a3983275ab1722
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-05-30 14:25:47 +0530

    Time: 30 ms (51.49%), Space: 13.4 MB (83.56%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> degree(numCourses, 0);
+        vector<vector<int>> graph(numCourses);
+        vector<int> finishedCourses;
+        
+        for(vector<int>& require : prerequisites){
+            int course = require[0];
+            int preCourse = require[1];
+            graph[preCourse].push_back(course);
+            degree[course] ++ ;
+        }
+        
+        std::queue<int> queue;  // will store the independent courses at any moment
+        for(int i = 0; i < numCourses; i++){
+            int deg = degree[i];
+            if(deg == 0)        // if the current course is independent, push into queue
+                queue.push(i);
+        }
+        
+        while(!queue.empty()){
+            int course = queue.front(); queue.pop();
+            degree[course]--;     // mark this course as done
+            finishedCourses.push_back(course); // push the course into the finished one
+            for(int dependent : graph[course]){
+                degree[dependent]--;
+                if(degree[dependent] == 0)
+                    queue.push(dependent);
+            }
+        }
+        
+        for(int i = 0; i < numCourses; i++)
+            if(degree[i] >= 0)
+                return vector<int>();
+        
+        return finishedCourses;
+    }
+};
