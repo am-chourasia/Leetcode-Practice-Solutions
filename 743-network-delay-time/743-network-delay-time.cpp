b208b3eaf58e35093f4456ae55d1b2f072757d7f
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-06-01 03:35:44 +0530

    Time: 112 ms (98.16%), Space: 39.9 MB (69.76%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,54 +1,42 @@
 class Solution {
 public:
-    vector<pair<int, int>> adj[101];
-    
-    void BFS(vector<int>& signalReceivedAt, int k) {
-        queue<int> q;
-        q.push(k);
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         
-        // Time for starting node is 0
-        signalReceivedAt[k] = 0;
+        vector<vector<pair<int, int>>> graph(n + 1);
+        for(vector<int>& edge : times){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            graph[u].push_back({v, w});
+        }
         
-        while (!q.empty()) {
-            int currNode = q.front(); 
-            q.pop();
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; 
+        // timing, node;
+        
+        vector<int> timing(n + 1, INT_MAX);
+        timing[k] = 0;
+        pq.push({0, k});
+        
+        while(not pq.empty()){
+            auto [currentTime, node] = pq.top();
+            pq.pop();
             
-            // Broadcast the signal to adjacent nodes
-            for (pair<int, int> edge : adj[currNode]) {
-                int time = edge.first;
-                int neighborNode = edge.second;
-                
-                int arrivalTime = signalReceivedAt[currNode] + time;
-                if (signalReceivedAt[neighborNode] > arrivalTime) {
-                    // Fastest signal time for neighborNode so far
-                    // signalReceivedAt[currNode] + time : 
-                    // time when signal reaches neighborNode
-                    signalReceivedAt[neighborNode] = arrivalTime;
-                    q.push(neighborNode);
+            for(auto& [neighbour, time] : graph[node]){
+                int newTime = time + currentTime;
+                if(newTime < timing[neighbour]){
+                    timing[neighbour] = newTime;
+                    pq.push({newTime, neighbour});
                 }
             }
         }
-    }
+        
+        int ans = -1;
+        for(int i = 1; i <= n; i++)
+            ans = max(ans, timing[i]);
+        
+        if(ans == INT_MAX)
+            return -1;
     
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        // Build the adjacency list
-        for (vector<int> time : times) {
-            int source = time[0];
-            int dest = time[1];
-            int travelTime = time[2];
-            
-            adj[source].push_back({travelTime, dest});
-        }
-
-        vector<int> signalReceivedAt(n + 1, INT_MAX);
-        BFS(signalReceivedAt, k);
-        
-        int answer = INT_MIN;
-        for (int i = 1; i <= n; i++) {
-            answer = max(answer, signalReceivedAt[i]);
-        }
-        
-        // INT_MAX signifies atleat one node is unreachable
-        return answer == INT_MAX ? -1 : answer;
+        return ans;
     }
 };
