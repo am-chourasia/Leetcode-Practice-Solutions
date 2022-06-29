1c0034f93813b03fd45a407b1e3495c7abd4fa5c
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-06-30 01:12:46 +0530

    Time: 181 ms (54.21%), Space: 11.6 MB (96.74%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -13,14 +13,8 @@ public:
         int index = -1;
         
         for(vector<int>& person : people){
-            queue.push_back(person);
-            index += 1;
             int frontCount = person[1];
-            int idx = index;
-            while(frontCount != idx){
-                swap(queue[idx], queue[idx - 1]);
-                idx -= 1;
-            }
+            queue.insert(queue.begin() + frontCount, person);
         }
         
         return queue;
