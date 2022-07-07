96a01569a78609cc6361cdacb4a0781c45f55766
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-07-08 00:56:23 +0530

    Time: 69 ms (47.05%), Space: 13.8 MB (99.89%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -4,23 +4,21 @@ class Solution:
         if len3 != len1 + len2:
             return False
         
-        dp = [[None for j in range(len2 + 1)] for i in range(len1 + 1)]
-        dp[len1][len2] = True
-        
-        k = len3 - 1
-        for i in range(len1 - 1, -1, -1):
-            dp[i][len2] = dp[i + 1][len2] and s[k] is s1[i]
-            k -= 1
+        dp = [None for j in range(len2 + 1)]
+        dp[len2] = True
         
         k = len3 - 1
         for j in range(len2 - 1, -1, -1):
-            dp[len1][j] = dp[len1][j + 1] and s[k] is s2[j]
+            dp[j] = dp[j + 1] and s[k] is s2[j]
             k -= 1
         
+        p = len3 - 1
         for i in range(len1 - 1, -1, -1):
+            dp[len2] = dp[len2] and s[p] is s1[i]
             for j in range(len2 - 1, -1, -1):
                 k = i + j
-                dp[i][j] = s[k] is s1[i] and dp[i + 1][j] or \
-                           s[k] is s2[j] and dp[i][j + 1]
+                dp[j] = s[k] is s1[i] and dp[j] or \
+                        s[k] is s2[j] and dp[j + 1]
+            p -= 1
             
-        return dp[0][0]
+        return dp[0]
