c3cb7b8c091e874e1f66ff7f99752405c4122540
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-07-22 21:04:57 +0530

    Time: 78 ms (65.63%), Space: 42.2 MB (46.11%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let start = 0, end = nums.length;
+    while(start < end){
+        let mid = Math.floor((start + end) / 2);
+        if(nums[mid] === target)
+            return mid;
+        if(nums[mid] > target)
+            end = mid;
+        else
+            start = mid + 1;
+    }
+    return start;
+};
