5072ad307e4796a85efef6c22b88dc1341573ccb
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-08-03 01:02:03 +0530

    Time: 309 ms (93.79%), Space: 132.1 MB (97.34%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,28 +1,34 @@
 class Solution {
+  void updateQueue(std::deque<pair<int, int>>& deque, int left, int right, int element) {
+    if(not deque.empty() and deque.front().second < left)
+      deque.pop_front();
+    while(not deque.empty() and deque.back().first <= element)
+      deque.pop_back();
+    deque.push_back({element, right});
+  }
+  
+  int maxElement(std::deque<pair<int, int>>& deque){
+    return deque.front().first;
+  }
+  
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int len = nums.size();
-        
-        multiset<int> maximum;
-        int left = 0, right = 0;
-        for(right = 0; right < k; right++)
-            maximum.insert(nums[right]);
-        
-        vector<int> slidingMax;
-        int maxi = *maximum.rbegin();
-        slidingMax.push_back(maxi);
-        
-        while(right < len){
-            int add = nums[right];
-            int remove = nums[left];
-            maximum.erase(maximum.find(remove));
-            maximum.insert(add);
-            maxi = *maximum.rbegin();
-            slidingMax.push_back(maxi);
-            right++;
-            left++;
+      int len = nums.size();
+      vector<int> sliding_max(len - k + 1);
+      int left = 0;
+      int right = 0;
+      
+      std::deque<pair<int, int>> deque;
+      
+      while (right < len) {
+        updateQueue(deque, left, right, nums[right]);
+        if (right >= k - 1) {
+          sliding_max[left] = maxElement(deque);
+          left += 1;
         }
-        
-        return slidingMax;
+        right += 1;
+      }
+      
+      return sliding_max;
     }
 };
