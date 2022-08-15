ee5caf3520c9fe76d1f2d3f9ca15b8c90afc7d76
Ashish Manoj Chourasia <ashish.official1202@gmail.com>
2022-08-15 12:27:44 +0530

    Time: 21 ms (89.69%), Space: 61.3 MB (73.61%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -1,51 +1,55 @@
+class ListNode{
+    int key, val;
+    ListNode next;
+    public ListNode(int key, int val){
+        this.key = key;
+        this.val = val;
+    }
+    public ListNode find(int keyToFind){
+        ListNode node = this, prev = null;
+        while(node != null && node.key != keyToFind){
+            prev = node;
+            node = node.next;
+        }
+        return prev;
+    }
+}
+
 class MyHashMap {
-        final ListNode[] nodes = new ListNode[10_000];
+    final ListNode[] nodes = new ListNode[10_000];
+    
+    int idx(int key) {
+        return Integer.hashCode(key) % nodes.length;
+    }
 
-        public void put(int key, int value){
-            int i = idx(key);
-            if(nodes[i] == null)
-                nodes[i] = new ListNode(-1, -1);
-            ListNode prev = find(nodes[i], key);
-            if(prev.next == null)
-                prev.next = new ListNode(key, value);
-            else prev.next.val = value;
+    public void put(int key, int value){
+        int i = idx(key);
+        if(nodes[i] == null)
+            nodes[i] = new ListNode(-1, -1);
+        ListNode prev = nodes[i].find(key);
+        if(prev.next == null)
+            prev.next = new ListNode(key, value);
+        else 
+            prev.next.val = value;
+    }
+
+    public int get(int key){
+        int i = idx(key);
+        if(nodes[i] == null)
+            return -1;
+        ListNode prev = nodes[i].find(key);
+        ListNode node = prev.next;
+        return node == null ? -1 : node.val;
+    }
+
+    public void remove(int key){
+        int i = idx(key);
+        if(nodes[i] != null){
+            ListNode prev = nodes[i].find(key);
+            if(prev.next != null)
+                prev.next = prev.next.next;
         }
-
-        public int get(int key){
-            int i = idx(key);
-            if(nodes[i] == null)
-                return -1;
-            ListNode node = find(nodes[i], key);
-            return node.next == null ? -1 : node.next.val;
-        }
-
-        public void remove(int key){
-            int i = idx(key);
-            if(nodes[i] != null){
-                ListNode prev = find(nodes[i], key);
-                if(prev.next != null)
-                    prev.next = prev.next.next;
-            }
-        }
-
-        int idx(int key){return Integer.hashCode(key) % nodes.length;}
-
-        ListNode find(ListNode bucket, int key){
-            ListNode node = bucket, prev = null;
-            for(; node != null && node.key != key; node = node.next)
-                prev = node;
-            return prev;
-        }
-
-        class ListNode{
-            int key, val;
-            ListNode next;
-
-            ListNode(int key, int val){
-                this.key = key;
-                this.val = val;
-            }
-        }
+    }
 }
 
 /**
